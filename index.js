04416a6adde921dc73de8602911225c6b2475e47
Benoit VALLON <benoitvallon@gmail.com>
2014-11-04 20:15:22 +0100

    Adds a logger and the rate limiting

--- index.js
+++ index.js
@@ -2,11 +2,35 @@ var koa = require('koa');
 var app = koa();
 var router = require('koa-router');
 var mount = require('koa-mount');
+var logger = require('koa-logger');
+var limit = require('koa-better-ratelimit');
 var api = require('./api/api.js');
 
 var APIv1 = new router();
 APIv1.get('/all', api.all);
 APIv1.get('/single', api.single);
+
+app.use(function *(next){
+    try{
+        yield next; //pass on the execution to downstream middlewares
+    } catch (err) { //executed only when an error occurs & no other middleware responds to the request
+        this.type = 'json'; //optional here
+        this.status = err.status || 500;
+        this.body = { 'error' : 'The application just went bonkers, hopefully NSA has all the logs ;) '};
+        //delegate the error back to application
+        this.app.emit('error', err, this);
+    }
+});
+
+// rate limiting
+app.use(limit({
+    duration: 1000*60*3 , // 3 min
+    max: 10,
+    blacklist: []
+}));
+
+// logging
+app.use(logger());
 
 var handler = function *(next){
     this.type = 'json';
