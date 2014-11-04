c61a6f5a779f83bb2fc2326bd84be8ebcfaa9603
Benoit VALLON <benoitvallon@gmail.com>
2014-11-04 20:12:29 +0100

    Adds the data and new routes

--- index.js
+++ index.js
@@ -2,15 +2,17 @@ var koa = require('koa');
 var app = koa();
 var router = require('koa-router');
 var mount = require('koa-mount');
+var api = require('./api/api.js');
+
+var APIv1 = new router();
+APIv1.get('/all', api.all);
+APIv1.get('/single', api.single);
 
 var handler = function *(next){
     this.type = 'json';
     this.status = 200;
     this.body = {'Welcome': 'This is a level 2 Hello World Application!!'};
 };
-
-var APIv1 = new router();
-APIv1.get('/all', handler);
 
 app.use(mount('/v1', APIv1.middleware()));
 if (!module.parent) app.listen(3000);
